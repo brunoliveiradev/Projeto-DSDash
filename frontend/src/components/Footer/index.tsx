4a9eebdccd632927c10fc884121982cb5cd365c0
BRUNOOLIVEIRA13 <bruno.henrique0liveir4@gmail.com>
2021-05-03 23:56:10 -0300

    :heavy_plus_sign: Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+function Footer() {
+  return (
+    <footer className="footer mt-auto py-3 bg-dark">
+      <div className="container">
+        <p className="text-light">App desenvolvido por <a href="https://github.com/BRUNOOLIVEIRA13" target="_blank" rel="noreferrer">Bruno Oliveira</a></p>
+        <p className="text-light"><small><strong>Spring React Week</strong><br />
+        Event provided by DevSuperior</small></p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
