305d6c901e48afc6bd1deb5c1d83476a3367f0c7
BRUNOOLIVEIRA13 <bruno.henrique0liveir4@gmail.com>
2021-05-08 19:32:21 -0300

    :heavy_plus_sign: Created Pagination

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,10 +1,14 @@
 import axios from "axios";
+import Pagination from "components/Pagination";
 import { useEffect, useState } from "react";
 import { MissionPage } from "types/mission";
 import { formatLocalDate } from "utils/format";
 import { BASE_URL } from "utils/requests";
 
 function DataTable() {
+
+  //Comunicação entre componentes
+  const [activePage, setActivePage] = useState(0);
 
   const [page, setPage] = useState<MissionPage>({
     first: true,
@@ -14,40 +18,49 @@ function DataTable() {
     totalPages: 0,
   });
 
+  //vai observar o activePage, sempre que mudar, busca da API a pagina atualizada
   useEffect(() => {
-    axios.get(`${BASE_URL}/missions?page=0&size=20&sort=date,desc`)
+    axios.get(`${BASE_URL}/missions?page=${activePage}&size=20&sort=date,desc`)
       .then(response => {
         setPage(response.data);
       })
 
-  }, []);
+  }, [activePage]);
+
+  const changePage = (index: number) => {
+    setActivePage(index);
+  }
+
 
   return (
-    <div className="table-responsive">
-      <table className="table table-striped table-sm table-hover table-dark">
-        <thead>
-          <tr className="text-secondary bg-info">
-            <th>Data</th>
-            <th>Jedi</th>
-            <th className="text-center">Spaceships</th>
-            <th className="text-center">Inimigos Eliminados</th>
-            <th>Baskar</th>
-          </tr>
-        </thead>
-        <tbody>
-          {page.content?.map(item => (
-            <tr key={item.id}>
-              <td>{formatLocalDate(item.date, "dd/MM/yyyy") }</td>
-              <td>{item.jedi.name}</td>
-              <td className="text-center">{item.visited}</td>
-              <td className="text-center">{item.deals}</td>
-              <td>{item.amount.toFixed(2)}</td>
+    <>
+    <Pagination page={page} onPageChange={changePage}/>
+      <div className="table-responsive">
+        <table className="table table-striped table-sm table-hover table-dark">
+          <thead>
+            <tr className="text-secondary bg-info">
+              <th>Data</th>
+              <th>Jedi</th>
+              <th className="text-center">Spaceships</th>
+              <th className="text-center">Inimigos Eliminados</th>
+              <th>Baskar</th>
             </tr>
-          ))}
+          </thead>
+          <tbody>
+            {page.content?.map(item => (
+              <tr key={item.id}>
+                <td>{formatLocalDate(item.date, "dd/MM/yyyy")}</td>
+                <td>{item.jedi.name}</td>
+                <td className="text-center">{item.visited}</td>
+                <td className="text-center">{item.deals}</td>
+                <td>{item.amount.toFixed(2)}</td>
+              </tr>
+            ))}
 
-        </tbody>
-      </table>
-    </div>
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 }
 
---
+++ frontend/src/components/Pagination/index.tsx
@@ -0,0 +1,33 @@
+import { MissionPage } from "types/mission";
+
+type Props = {
+  page: MissionPage;
+  //Faz parte da comunicação entre componentes
+  onPageChange: Function;
+}
+
+const Pagination = ({ page, onPageChange } : Props) => {
+
+  return (
+    <div className="row d-flex justify-content-center">
+      <nav>
+        <ul className="pagination">
+          <li className={`page-item ${page.first ? 'disabled' : ''}`}>
+            <button className="page-link" onClick={() => onPageChange(page.number - 1)}>Anterior</button>
+          </li>
+
+          <li className="page-item disabled">
+            <span className="page-link">{page.number + 1}</span>
+          </li>
+
+          <li className={`page-item ${page.last ? 'disabled' : ''}`}>
+            <button className="page-link" onClick={() => onPageChange(page.number + 1)}>Proxima</button>
+          </li>
+        </ul>
+      </nav>
+    </div>
+
+  )
+}
+
+export default Pagination;
